433e99566250f26d6e02fa701e2de413456d80ab
Farukh-Habib <85098233+Farukh-Habib@users.noreply.github.com>
2021-06-16 12:39:30 +0530

    Add files via upload

---
+++ Automate Whatsapp.py
@@ -0,0 +1,15 @@
+import pywhatkit
+
+# AUTOMATE SENDING MSG TO WHATSAPP
+# pywhatkit.sendwhatmsg('+919372972088','alert203 ',12,23)  
+
+# DIRECTLY GO TO THE YT 
+# pywhatkit.playonyt('github') 
+
+# directly go to google
+# pywhatkit.search('hello world')
+
+
+# chech windows profile
+pywhatkit.check_window()                              
+
