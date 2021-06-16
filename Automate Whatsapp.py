4a5a316d5a92e7b41b49ccfee4227fccd34babd1
Farukh-Habib <85098233+Farukh-Habib@users.noreply.github.com>
2021-06-16 12:40:06 +0530

    Update Automate Whatsapp.py

--- Automate Whatsapp.py
+++ Automate Whatsapp.py
@@ -1,15 +1,15 @@
 import pywhatkit
 
-# AUTOMATE SENDING MSG TO WHATSAPP
-# pywhatkit.sendwhatmsg('+919372972088','alert203 ',12,23)  
+AUTOMATE SENDING MSG TO WHATSAPP
+pywhatkit.sendwhatmsg('+919372972088','alert203 ',12,23)  
 
-# DIRECTLY GO TO THE YT 
-# pywhatkit.playonyt('github') 
+DIRECTLY GO TO THE YT 
+pywhatkit.playonyt('github') 
 
-# directly go to google
-# pywhatkit.search('hello world')
+directly go to google
+pywhatkit.search('hello world')
 
 
-# chech windows profile
-# pywhatkit.check_window()                              
+chech windows profile
+pywhatkit.check_window()                              
 
